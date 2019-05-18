e0c720b56dc8d100c2939ab4363771754de84b36
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-18 11:05:31 +0900

    feat: noteモデルのテスト

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,64 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  it "検索文字列に一致するメモを返すこと" do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: "taro",
       last_name: "jiro",
       email: "tester@example.com",
       password: "password",
     )
 
-    project = user.projects.create(
+    @project = @user.projects.create(
       name: "test project",
     )
-
-    note1 = project.notes.create(
-      message: "first test",
-      user: user,
-    )
-
-    note2 = project.notes.create(
-      message: "second test",
-      user: user,
-    )
-
-    note3 = project.notes.create(
-      message: "First test",
-      user: user,
-    )
-
-    expect(Note.search("first")).to include(note1,note3)
-    expect(Note.search("first")).to_not include(note2)
   end
 
-  it "検索結果が1件も見つからなければ空のコレクションを返すこと" do
-    user = User.create(
-      first_name: "taro",
-      last_name: "jiro",
-      email: "tester@example.com",
-      password: "password",
+  it "ユーザー、プロジェクト、メッセージがあれば有効な状態であること" do
+    note = Note.new(
+      message: "This is a sample",
+      user: @user,
+      project: @project,
     )
 
-    project = user.projects.create(
-      name: "test project",
-    )
+    expect(note).to be_valid
+  end
 
-    note1 = project.notes.create(
-      message: "first test",
-      user: user,
-    )
+  it "メッセージがなければ無効な状態であること" do
+    note = Note.new(message: nil)
+    note.valid?
+    expect(note.errors[:message]).to include("can't be blank")
+  end
 
-    note2 = project.notes.create(
-      message: "second test",
-      user: user,
-    )
+  describe "文字列に一致するメッセージを検索" do
+    before do
+      @note1 = @project.notes.create(
+        message: "first test",
+        user: @user,
+      )
 
-    note3 = project.notes.create(
-      message: "First test",
-      user: user,
-    )
+      @note2 = @project.notes.create(
+        message: "second test",
+        user: @user,
+      )
 
-    expect(Note.search("message")).to be_empty
+      @note3 = @project.notes.create(
+        message: "First test",
+        user: @user,
+      )
+    end
+
+    context "一致するデータが見つかるとき" do
+      it "検索文字列に一致するメモを返すこと" do
+        expect(Note.search("first")).to include(@note1,@note3)
+      end
+    end
+
+    context "一致するデータが1件も見つからないとき" do
+      it "空のコレクションを返すこと" do
+        expect(Note.search("message")).to be_empty
+      end
+    end
   end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,49 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "ユーザー単位では重複したプロジェクト名を許可しないこと" do
-    user = User.create(
+  before do
+    @user = User.create(
       first_name: "Joe",
       last_name: "taro",
       email: "joetester@example.com",
       password: "password",
     )
 
-    user.projects.create(
+    @project = @user.projects.create(
       name: "Test Project",
     )
 
-    new_project = user.projects.build(
-      name: "Test Project",
-    )
-
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
-
-  it "二人のユーザーが同じ名前を使うことは許可すること" do
-    user = User.create(
-      first_name: "Joe",
-      last_name: "taro",
-      email: "joetester@example.com",
-      password: "password",
-    )
-
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    other_user = User.create(
+    @other_user = User.create(
       first_name: "taro",
       last_name: "jiro",
       email: "tester@example.com",
       password: "password",
     )
 
-    other_project = other_user.projects.build(
+    @other_project = @other_user.projects.build(
       name: "Test Project",
     )
+  end
+  it "ユーザー単位では重複したプロジェクト名を許可しないこと" do
+    new_project = @user.projects.build(
+      name: "Test Project",
+    )
+    new_project.valid?
+    expect(new_project.errors[:name]).to include("has already been taken")
+  end
 
-    expect(other_project).to be_valid
+  it "二人のユーザーが同じ名前を使うことは許可すること" do
+    expect(@other_project).to be_valid
+  end
+
+  it "名前が空のプロジェクトは許可しない" do
+    project = Project.new(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
   end
 end
